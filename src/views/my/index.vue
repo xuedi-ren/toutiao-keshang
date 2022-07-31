4a3cca614c3fe17c6d6b255b4b6706c8164bda6c
Re <1686262932@qq.com>
2022-07-31 11:22:36 +0800

    我的界面布局完成

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>俺是我的</div>
+  <div>
+    <!-- ! 头部区域 -->
+    <header>
+      <div class="login" v-if="isLogin">已登陆</div>
+      <div class="logout" v-else>未登陆</div>
+    </header>
+    <!-- ! 中间 -->
+    <main>收藏/历史/消息通知/小智同学</main>
+    <!-- ! 底部 -->
+    <footer>
+      <button v-if="isLogin" @click="logout">退出</button>
+    </footer>
+  </div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    isLogin() {
+      return !!this.$store.state.tokenObj.token
+    }
+  },
+  methods: {
+    logout() {
+      this.$dialog
+        .confirm({
+          title: '标题',
+          message:
+            '如果解决方法是丑陋的，那就肯定还有更好的解决方法，只是还没有发现而已。'
+        })
+        .then(() => {
+          this.$store.commit('SET_TOKEN', {})
+        })
+        .catch(() => {
+          // on cancel
+        })
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
